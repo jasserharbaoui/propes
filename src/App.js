c668f9d7b7b2a8349595e95c1cad5a46b744c6a4
harbaouijasser <harbaouijasser77@gmil.com>
2024-03-29 13:11:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import PlayersList from './component/PlayersList';
+
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
